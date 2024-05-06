f7310c09cfacb8c51037e480cf4c7a57149800d0
nick <no-spam@github.is.public.info>
2024-05-05 18:26:50 -0700

    Strip wasm

--- src/app.rs
+++ src/app.rs
@@ -45,17 +45,12 @@ impl eframe::App for WhiteboardState {
             // The top panel is often a good place for a menu bar:
 
             egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                    }
+                });
+                ui.add_space(16.0);
                 egui::widgets::global_dark_light_mode_buttons(ui);
             });
         });
